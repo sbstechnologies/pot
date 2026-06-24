cb1c31ac79596c776c2d916594a9898fcc7845af
Babu Subramanian <softdreams4u@gmail.com>
2026-06-24 19:39:11 +0530

    Enter

--- app/components/ComplianceNotice.tsx
+++ app/components/ComplianceNotice.tsx
@@ -1,13 +1,13 @@
 export default function ComplianceNotice() {
   return (
-    <div className="bg-[#0f1e48] px-6 xs:px-6 sm:px-6 md:px-20 lg:px-40 xl:px-40 xxl:px-80 mx-auto py-5">
-      <div className="mx-auto border border-style:solid border-width: 16px  border-[#24468d] flex max-w-[1920px] flex-col gap-6 rounded-[26px] bg-[#0f1e48] px-8 py-4 md:px-12 md:py-9 lg:flex-row lg:items-center lg:justify-between">
+    <div className="bg-[#0f1e48] px-6 xs:px-6 sm:px-6 md:px-20 lg:px-40 xl:px-40 xxl:px-80 mx-auto py-4">
+      <div className="mx-auto border border-style:solid border-width: 16px  border-[#24468d] flex max-w-[1920px] flex-col gap-6 rounded-[26px] bg-[#0f1e48] px-8 md:px-12 py-8 lg:flex-row lg:items-center lg:justify-between">
         <div>
           <h2 className="font-[Instrument_Serif] text-[24px] leading-none text-[#8e99ad]">
             Equal Housing Opportunity
           </h2>
 
-          <p className="font-[Plus_Jakarta_Sans] mt-3 text-[15px] text-[#8e99ad]">
+          <p className="font-[Plus_Jakarta_Sans] mt-4 text-[15px] text-[#8e99ad]">
             Our community proudly welcomes Housing Choice Voucher households. We
             market all available restricted and non-restricted units to voucher
             holders and notify local housing authorities of voucher acceptance,
@@ -15,9 +15,6 @@ export default function ComplianceNotice() {
             affirmative marketing policies.
           </p>
         </div>
-
-        {/* STYLED BUTTON CONTAINER TO MATCH ATTACHED DESIGN */}
-        <div className="shrink-0"></div>
       </div>
     </div>
   );
